5e70efe2b2e99a931151d28f150b5cc739eff95f
Matteo Valenzi <dilloawallace@hotmail.com>
2024-09-26 20:26:05 +0200

    .

--- src/routes/pages.ts
+++ src/routes/pages.ts
@@ -4,9 +4,7 @@ import fs from "node:fs";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-    fs.writeFileSync("pippo.txt", "pippo");
-    res.sendFile("pippo.txt");
-    // res.send("Benvenuti sul server");
+    res.send("Benvenuti sul server");
 });
 
 router.get("/guida", (req: Request, res: Response) => {
