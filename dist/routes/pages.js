85d1f3aa239d0a83a723ff139714a9276502ea38
Matteo Valenzi <dilloawallace@hotmail.com>
2024-09-26 20:24:15 +0200

    file test

--- dist/routes/pages.js
+++ dist/routes/pages.js
@@ -1,9 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const node_fs_1 = __importDefault(require("node:fs"));
 const router = (0, express_1.Router)();
 router.get("/", (req, res) => {
-    res.send("Benvenuti sul server");
+    node_fs_1.default.writeFileSync("pippo.txt", "pippo");
+    res.sendFile("pippo.txt");
+    // res.send("Benvenuti sul server");
 });
 router.get("/guida", (req, res) => {
     res.send("La guida al server");
--- src/routes/pages.ts
+++ src/routes/pages.ts
@@ -1,9 +1,12 @@
 import { Request, Response, Router } from "express";
+import fs from "node:fs";
 
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-    res.send("Benvenuti sul server");
+    fs.writeFileSync("pippo.txt", "pippo");
+    res.sendFile("pippo.txt");
+    // res.send("Benvenuti sul server");
 });
 
 router.get("/guida", (req: Request, res: Response) => {
